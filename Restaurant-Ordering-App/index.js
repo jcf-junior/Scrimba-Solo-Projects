f6633a27c2b6e07a7fe1156e0500b66483fe355f
JJ <h1ddenntc@gmail.com>
2024-11-25 20:27:34 +0000

    .

---
+++ Restaurant-Ordering-App/index.js
@@ -0,0 +1,21 @@
+import menuArray from '/data.js'
+
+const menuSection = document.getElementById('menu-section')
+
+
+function getMenuItemsHtml(menuArray) {
+
+    menuArray.map(function(menuItem){
+        const {name, ingredients, id, price, emoji} = menuItem
+        
+    })
+
+}
+
+
+function renderItems() {
+
+    return `
+
+    `
+}
