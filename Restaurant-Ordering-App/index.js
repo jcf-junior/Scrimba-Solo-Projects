296bd76cca485f6a120be321fcfdd3eb715cecb9
JJ <h1ddenntc@gmail.com>
2024-11-29 19:20:21 +0000

    i dont even know anymore

--- Restaurant-Ordering-App/index.js
+++ Restaurant-Ordering-App/index.js
@@ -71,3 +71,27 @@ function renderItems(htmlToRender) {
 }
 
 getMenuItemsHtml(menuArray)
+
+
+// const checkoutSection = document.getElementById('checkout-section')
+// const orderConfirmation = document.getElementById('order-confirmation')
+// const paymentModal = document.getElementById('payment-modal')
+// const modalPayBtn = document.getElementById('modal-pay-btn')
+
+// // If user has selected 1 item === true: classlist.toggle('hidden')
+
+// let orderPaid = false
+
+// modalPayBtn.addEventListener('click', function(){
+//     checkoutSection.classList.add('hidden')
+//     paymentModal.classList.add('hidden')
+//     orderConfirmation.classList.remove('hidden')
+//     console.log('order paid is now true');
+// })
+
+// if (orderPaid === true) {
+//     checkoutSection.classList.add('hidden')
+//     paymentModal.classList.add('hidden')
+//     orderConfirmation.classList.remove('hidden')
+//     console.log('order paid is now true');
+// }
